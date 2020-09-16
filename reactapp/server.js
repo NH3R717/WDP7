d35bce4056361a816069df6d412a853457c7ac44
“Tommy” <nh3r717@gmail.com>
2020-09-16 13:06:12 -0400

    server.js – heroku

--- reactapp/server.js
+++ reactapp/server.js
@@ -4,6 +4,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
