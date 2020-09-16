4a7f42cfb74d354b20d557218b695fa98390b5bc
brandonbrown <brandon@centaflux.com>
2020-09-16 16:06:21 -0400

    add build path

--- api/server.js
+++ api/server.js
@@ -4,9 +4,12 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+// Serve any static files
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// Handle React routing, return all requests to React app
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.use(bodyParser.json());
