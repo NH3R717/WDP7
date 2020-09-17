68e6e8e33895d27de77da6a3404f609b029bd8bc
“Tommy” <nh3r717@gmail.com>
2020-09-16 20:51:12 -0400

    update – component/route structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import styles from './App.css';
 
+// route imports
 import Header from './components/sub/header';
 import Login from './components/view/login';
 import Register from './components/view/login';
