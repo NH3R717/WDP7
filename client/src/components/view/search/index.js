38b86e5562acba28acda52289f4c7ae826a6c4d7
“Tommy” <nh3r717@gmail.com>
2020-09-19 15:35:51 -0400

    search format

--- client/src/components/view/search/index.js
+++ client/src/components/view/search/index.js
@@ -1,19 +1,75 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Input, Container, Label } from 'reactstrap';
 import PropTypes from 'prop-types';
 import styles from './styles.module.css';
 
-class Default_Comp extends Component {
+class Search extends Component {
   render() {
-    return <h1 className={styles.defaultTextStyle}>Search</h1>;
+    return (
+      <section>
+        <Container className={styles.form_container}>
+          <div className={styles.form_box}>
+            <h2>Search</h2>
+            <Form>
+              <FormGroup className={styles.form_group}>
+                <Input
+                  className={styles.input}
+                  type="text"
+                  name="search"
+                  id="search"
+                  placeholder="Search Term"
+                />
+              </FormGroup>
+              <FormGroup className={styles.form_group}>
+                {/* <Label>Begining Date</Label> */}
+                <h4>Begining Date</h4>
+                <Input
+                  className={styles.input}
+                  type="date"
+                  name="begin_date"
+                  id="begin_date"
+                  placeholder="Begin Date"
+                />
+              </FormGroup>
+              <FormGroup>
+                <h4>End Date</h4>
+                <Input
+                  className={styles.input}
+                  type="date"
+                  name="end_date"
+                  id="end_date"
+                  placeholder="End Date"
+                />
+              </FormGroup>
+              {/* <FormGroup>
+              <Input
+                className={styles.input}
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search"
+              />
+            </FormGroup> */}
+              {/* <Button className={styles.left_button + ' ' + styles.button}>
+                Register
+              </Button> */}
+              <Button className={styles.right_button + ' ' + styles.button}>
+                Search
+              </Button>
+            </Form>
+          </div>
+        </Container>
+      </section>
+    );
   }
 }
 
-Default_Comp.propTypes = {
+Search.propTypes = {
   id: PropTypes.string,
 };
 
-Default_Comp.defaultProps = {
+Search.defaultProps = {
   choice: {},
 };
 
-export default Default_Comp;
+export default Search;
