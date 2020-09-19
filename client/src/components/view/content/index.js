2e1e2a6714831304fc6d30667a16e7cc17d4b31d
“Tommy” <nh3r717@gmail.com>
2020-09-19 18:44:55 -0400

    add – notification card

--- client/src/components/view/content/index.js
+++ client/src/components/view/content/index.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  CardLink,
+} from 'reactstrap';
 import PropTypes from 'prop-types';
 import styles from './styles.module.css';
 
 class Default_Comp extends Component {
   render() {
     return (
-      <h1 className={styles.defaultTextStyle}>
-        Content – Notification/DM/Profile
-      </h1>
+      <section className={styles.card_container}>
+        {/* notification */}
+        <Card>
+          <div>
+            <CardBody className={styles.card_head}>
+              <img
+                className={styles.avatar_img}
+                src="/avatar_blue.png"
+                alt="Card image cap"
+              />
+              <CardTitle className={styles.notification_message}>
+                This is a notification, letting you know.
+              </CardTitle>
+              <img
+                className={styles.card_thumb}
+                src="/sample_image.jpg"
+                alt="Card image cap"
+              />
+              {/* <CardSubtitle>Card subtitle</CardSubtitle> */}
+            </CardBody>
+          </div>
+          <CardBody>
+            <CardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </CardText>
+            <CardLink href="#">Card Link</CardLink>
+            <CardLink href="#">Another Link</CardLink>
+          </CardBody>
+        </Card>
+      </section>
     );
   }
 }
