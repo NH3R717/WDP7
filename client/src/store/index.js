bef84f482b85022ae1570db4d1fd2137f8672629
“Tommy” <nh3r717@gmail.com>
2020-09-23 11:24:21 -0400

    update store index – reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,9 +5,19 @@ import { createLogger } from 'redux-logger';
 import callAPI from './utils/callAPIMiddleware';
 
 // import of reducers
+import notifications from './notifications/reducer';
+import direct_messages from './direct_messages/reducer';
+import images from './images/reducer';
+import audio from './audio/reducer';
+import video from './video/reducer';
 
 const rootReducer = combineReducers({
   //list all imported reducers
+  notifications,
+  direct_messages,
+  images,
+  audio,
+  video,
 });
 
 const middleware = applyMiddleware(thunkMiddleware, callAPI, createLogger());
