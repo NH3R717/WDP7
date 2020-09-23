bb68d0d4662ff2191c9c5d7c039704de03f1db18
“Tommy” <nh3r717@gmail.com>
2020-09-23 11:18:28 -0400

    update – actiontypes

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,7 +4,14 @@ import { createLogger } from 'redux-logger';
 
 import callAPI from './utils/callAPIMiddleware';
 
-const rootReducer = combineReducers({});
+// import of reducers
+
+const rootReducer = combineReducers({
+  //list all imported reducers
+});
 
 const middleware = applyMiddleware(thunkMiddleware, callAPI, createLogger());
+
+// export const store = createStore(rootReducer, middleware);
+
 export default createStore(rootReducer, middleware);
