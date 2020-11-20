fa0a4068819a51a70d8b56880268f2f9430cf36c
“Tommy” <nh3r717@gmail.com>
2020-11-20 09:50:23 -0500

    update – A202... seeder file

--- api/db/seeders/A20201029174114-Notifications.js
+++ api/db/seeders/A20201029174114-Notifications.js
@@ -7,7 +7,7 @@ module.exports = {
       [
         {
           id: "f9700fa5-e3b2-4c5d-9fc3-10b883b84fcc",
-          userId: "92061fc4-690a-47ba-b3af-64cd6a8909da",
+          usersId: "92061fc4-690a-47ba-b3af-64cd6a8909da",
           notificationId: "e2d9204d-5a94-43c4-8f57-5b492edd4717",
           notificationsTextsId: "7465fd03-27a0-49cc-bd0c-6692a1067bc6",
           // TODO – to get new fk ids to populate
