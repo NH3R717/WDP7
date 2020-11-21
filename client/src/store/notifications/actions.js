351c18fa811ce19b53bfacfed2b6741f6425bb65
“Tommy” <nh3r717@gmail.com>
2020-11-21 16:19:16 -0500

    update – CRUD for notification from client

--- client/src/store/notifications/actions.js
+++ client/src/store/notifications/actions.js
@@ -160,7 +160,7 @@ export const deleteNotification = (notification) => {
     DELETE_NOTIFICATION_ERROR,
   ],
   // callAPI: () => API.get(`/notifications`),
-  callAPI: () => API.delete(`/notifications/${notification}`, notification),
+  callAPI: () => API.delete(`/notifications/${notification.id}`, notification),
   payload: { id: notification.id }
   
 }
